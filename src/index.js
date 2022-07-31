810fe3e56e62eb8013f4013d9fe2af95cafd3ce3
Pavel Lebedz <lebedz.pavel@gmail.com>
2022-07-31 13:10:00 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if (n > 0) {
+        return Number(n.toString().split('').reverse().join(''));
+    } else {
+        return Number(Math.abs(n).toString().split('').reverse().join(''));
+    }
 }
